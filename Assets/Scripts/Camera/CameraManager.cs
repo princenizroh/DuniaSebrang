9c9f10111df6bc644ebd7af0b18e435c0fb6ed21
princenizroh <princenizroh7@gmail.com>
2025-02-07 01:05:41 +0800

    new branch

---
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+// using Cinemachine;
+
+public class CameraManager : MonoBehaviour
+{
+//     public CinemachineVirtualCamera[] cameras;
+//     public CinemachineVirtualCamera startCamera;
+//     private CinemachineVirtualCamera currentCam;
+
+    private void Start()
+    {
+//         // Set kamera awal
+//         currentCam = startCamera;
+
+//         for (int i = 0; i < cameras.Length; i++)
+//         {
+//             cameras[i].Priority = (cameras[i] == currentCam) ? 20 : 10;
+//         }
+    }
+
+//     public void SwitchCamera(int cameraIndex)
+//     {
+//         // Validasi indeks kamera
+//         if (cameraIndex < 0 || cameraIndex >= cameras.Length)
+//         {
+//             Debug.LogError($"Kamera dengan indeks {cameraIndex} tidak valid!");
+//             return;
+//         }
+
+//         // Pindahkan ke kamera baru
+//         currentCam = cameras[cameraIndex];
+//         for (int i = 0; i < cameras.Length; i++)
+//         {
+//             cameras[i].Priority = (i == cameraIndex) ? 20 : 10;
+//         }
+//     }
+}
---
+++ Assets/Scripts/Camera/CameraTrigger.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    public int cameraIndex; // Indeks kamera yang akan diaktifkan
+    // private CameraManager cameraManager;
+
+    // private void Start()
+    // {
+    //     cameraManager = FindObjectOfType<CameraManager>();
+    // }
+
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     if (other.CompareTag("Player"))
+    //     {
+    //         Debug.Log($"Player masuk ke trigger dengan kamera indeks {cameraIndex}");
+    //         cameraManager.SwitchCamera(cameraIndex);
+    //     }
+    // }
+}
