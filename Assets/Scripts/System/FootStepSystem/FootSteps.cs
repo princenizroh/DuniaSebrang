ad872be020a2971e571e15fc3f45cd6d84fdac62
princenizroh <princenizroh7@gmail.com>
2025-04-21 01:45:47 +0800

    Setup

--- Assets/Scripts/System/FootStepSystem/FootSteps.cs
+++ Assets/Scripts/System/FootStepSystem/FootSteps.cs
@@ -8,9 +8,9 @@ namespace DS
     public class FootSteps : MonoBehaviour
     {
         [SerializeField] ParticleSystem footStepParticle;
-        public float delta = 1f;
+        public float delta = 0.7f;
         Vector3 lastEmit;
-        [SerializeField] float gap = 0.5f;
+        [SerializeField] float gap = 0.1f;
         [SerializeField] int dir = 1;
         void Start()
         {
